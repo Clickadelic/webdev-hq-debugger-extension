e33849b9ec2ae587b951336742863e0ba91610d6
Clickadelic <toby.hopp@gmail.com>
2026-09-07 02:41:34 +0200

    Commit test

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,14 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
+	modules: ["@wxt-dev/module-react"],
+	manifest: {
+		default_locale: "en",
+		permissions: ["contextMenus", "cookies", "downloads", "history", "management", "tabs", "tabGroups", "topSites", "storage", "sidePanel", "scripting"],
+		host_permissions: ["https://webdev-hq.com/*"],
+		background: {
+			service_worker: "background.js"
+		}
+	}
 });
